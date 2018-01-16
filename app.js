55833b22ec3f10f68a40dd8f26333bdbab6c66b7
TomIrimescu <tom@irimescu.com>
2018-01-16 18:41:41 -0500

    decorator prove outs, added experimental decorators

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function logged(constructorFn) {
+    console.log(constructorFn);
+}
+var Person = /** @class */ (function () {
+    function Person() {
+        console.log("Hi!");
+    }
+    Person = __decorate([
+        logged
+    ], Person);
+    return Person;
+}());
+// Factory
+function logging(value) {
+    return value ? logged : function () { return null; };
+}
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car = __decorate([
+        logging(true)
+    ], Car);
+    return Car;
+}());
+// Advanced
+function printable(constructorFn) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    };
+}
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this.name = "Green Plant";
+    }
+    Plant = __decorate([
+        logging(false),
+        printable
+    ], Plant);
+    return Plant;
+}());
+var plant = new Plant();
+plant.print();
+// Method Decorator
+// Property Decorator
+function editable(value) {
+    return function (_target, _propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+/*function overwritable(value: boolean) {
+  return function (target: any, propName: string): any {
+    const newDescriptor: PropertyDescriptor = {
+      writable: value
+    };
+    return newDescriptor;
+  }
+}*/
+var Project = /** @class */ (function () {
+    function Project(name) {
+        this.projectName = name;
+    }
+    Project.prototype.calcBudget = function () {
+        console.log(1000);
+    };
+    __decorate([
+        editable(true)
+    ], Project.prototype, "calcBudget", null);
+    return Project;
+}());
+var project = new Project("Super Project");
+project.calcBudget();
+project.calcBudget = function () {
+    console.log(2000);
+};
+project.calcBudget();
+console.log(project);
