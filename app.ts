0af1051b7901fa54a11b52054318a7509fce4f47
TomIrimescu <tom@irimescu.com>
2018-01-16 21:50:04 -0500

    further decorator prove outs

--- app.ts
+++ app.ts
@@ -16,7 +16,6 @@ function logging(value: boolean) {
 
 @logging(true)
 class Car {
-
 }
 
 // Advanced
@@ -31,36 +30,37 @@ function printable(constructorFn: Function) {
 class Plant {
   name = "Green Plant";
 }
+
 const plant = new Plant();
 (<any>plant).print();
 
 
 // Method Decorator
-// Property Decorator
 function editable(value: boolean) {
   return function (_target: any, _propName: string, descriptor: PropertyDescriptor) {
     descriptor.writable = value;
   }
 }
 
-/*function overwritable(value: boolean) {
-  return function (target: any, propName: string): any {
+// Property Decorator
+function overwritable(value: boolean) {
+  return function (_target: any, _propName: string): any {
     const newDescriptor: PropertyDescriptor = {
       writable: value
     };
     return newDescriptor;
   }
-}*/
+}
 
 class Project {
-/*  @overwritable(false)*/
+  @overwritable(true) // property decorator
   projectName: string;
   
   constructor(name: string) {
     this.projectName = name;
   }
   
-  @editable(true)
+  @editable(true) // method decorator
   calcBudget() {
     console.log(1000);
   }
@@ -74,7 +74,31 @@ project.calcBudget = function () {
 project.calcBudget();
 console.log(project);
 
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+  console.log("Target: ", target);
+  console.log("methodName: ", methodName);
+  console.log("paramIndex: ", paramIndex);
+}
 
+class Course {
+  name: string;
+  
+  constructor(name: string) {
+    this.name = name;
+  }
+  
+  printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+    if (printAll) {
+      console.log(10000);
+    } else {
+      console.log(2000);
+    }
+  }
+}
+const course = new Course("Super Course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
 
 
 
